836c6c199ae25aa6b4adf24a21e4695f4af1827f
Thunay Moreira de Soares <thunay@buildbox.com.br>
2024-05-25 20:25:02 -0300

    ✨ Home - Services

--- pages/home/_index.php
+++ pages/home/_index.php
@@ -11,4 +11,6 @@ get_template_part('pages/home/components/top-hero');
 
 get_template_part('pages/home/components/about-me');
 
+get_template_part('pages/home/components/services');
+
 get_template_part('components/footer');

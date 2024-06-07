46d71906b185fdc104ffa1d405096fcadd6f3cb9
Thunay Moreira de Soares <thunay@buildbox.com.br>
2024-06-07 01:25:59 -0300

    🔥 ACF - Background Image

--- includes/entities/ThemeOptions/Register.php
+++ includes/entities/ThemeOptions/Register.php
@@ -66,51 +66,6 @@ class ThemeOptionsRegister
 
         $Hero->add_fields([
             [
-                'key' => 'field_665676efbf8d8',
-                'label' => 'Imagem destacada',
-                'name' => '',
-                'aria-label' => '',
-                'type' => 'tab',
-                'instructions' => '',
-                'required' => 0,
-                'conditional_logic' => 0,
-                'wrapper' => array(
-                    'width' => '',
-                    'class' => '',
-                    'id' => '',
-                ),
-                'placement' => 'top',
-                'endpoint' => 0,
-            ],
-            [
-                'key' => 'field_665676be28d35',
-                'label' => 'Imagem',
-                'name' => 'home_hero_featured_image',
-                'aria-label' => '',
-                'type' => 'image',
-                'instructions' => '',
-                'required' => 0,
-                'conditional_logic' => 0,
-                'wrapper' => array(
-                    'width' => '',
-                    'class' => '',
-                    'id' => '',
-                ),
-                'return_format' => 'url',
-                'library' => 'all',
-                'min_width' => '',
-                'min_height' => '',
-                'min_size' => '',
-                'max_width' => '',
-                'max_height' => '',
-                'max_size' => '',
-                'mime_types' => '',
-                'preview_size' => 'medium',
-            ],
-        ]);
-
-        $Hero->add_fields([
-            [
                 'key' => 'field_66566f9e3d181',
                 'label' => 'Título e descrição',
                 'name' => '',

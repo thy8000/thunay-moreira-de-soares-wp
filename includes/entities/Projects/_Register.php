5abdceb0bab5a444c6d9a4f5ba00886034ffdc3a
Thunay Moreira de Soares <thunay@buildbox.com.br>
2024-06-25 22:08:41 -0300

    🩹 Projects: Register class - category doesn't show in post editor

--- includes/entities/Projects/_Register.php
+++ includes/entities/Projects/_Register.php
@@ -9,6 +9,7 @@ class Projects_Register
     public function __construct()
     {
         add_action('init', [$this, 'register_post_type']);
+        add_action('init', [$this, 'register_taxonomy']);
     }
 
     public function register_post_type()
@@ -59,7 +60,33 @@ class Projects_Register
 
         register_post_type('projects', $args);
     }
-}
 
+    public function register_taxonomy()
+    {
+        $args = [
+            'labels' => [
+                'name'              => _x('Categorias do Projeto', 'Categoria do Projeto'),
+                'singular_name'     => _x('Categoria do Projeto', 'Categoria do Projeto'),
+                'search_items'      => __('Buscar Categorias do Projeto'),
+                'all_items'         => __('Todas Categorias do Projeto'),
+                'parent_item'       => __('Categoria do Projeto Pai'),
+                'parent_item_colon' => __('Categoria do Projeto Pai:'),
+                'edit_item'         => __('Editar Categoria do Projeto'),
+                'update_item'       => __('Atualizar Categoria do Projeto'),
+                'add_new_item'      => __('Adicionar Nova Categoria do Projeto'),
+                'new_item_name'     => __('Nome da Nova Categoria do Projeto'),
+                'menu_name'         => __('Categoria do Projeto'),
+            ],
+            'hierarchical'      => true,
+            'show_ui'           => true,
+            'show_admin_column' => true,
+            'show_in_rest'      => true,
+            'query_var'         => true,
+            'rewrite'           => ['slug' => 'project-category'],
+        ];
+
+        register_taxonomy('project_category', ['projects'], $args);
+    }
+}
 
 new Projects_Register();

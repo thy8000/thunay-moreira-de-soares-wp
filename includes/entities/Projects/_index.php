f2f40269588260485f95cbfbcff5c9ea4c919954
Thunay Moreira de Soares <thunay@buildbox.com.br>
2024-06-25 21:56:43 -0300

    🏗️ ✨ Projects: ACF_Register & Register Classes

--- includes/entities/Projects/_index.php
+++ includes/entities/Projects/_index.php
@@ -4,4 +4,5 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
-require_once __DIR__ . DIRECTORY_SEPARATOR . 'Register.php';
+require_once __DIR__ . DIRECTORY_SEPARATOR . '_Register.php';
+require_once __DIR__ . DIRECTORY_SEPARATOR . 'ACF_Register.php';

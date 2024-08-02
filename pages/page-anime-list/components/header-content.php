c23ee0326134e1d7b975c93b71800078096bd1ba
Thunay Moreira de Soares <thunay@buildbox.com.br>
2024-08-01 21:36:11 -0300

    ✨ Header content copyright

--- pages/page-anime-list/components/header-content.php
+++ pages/page-anime-list/components/header-content.php
@@ -15,11 +15,17 @@ try {
 ?>
 
 <section>
-    <div class="flex">
+    <div class="flex flex-col">
         <div class="flex justify-center items-center flex-col gap-4 mt-20 bg-neutral-800 p-10 rounded-lg border border-neutral-700 w-full">
             <h2 class="text-center text-3xl font-poppins font-semibold text-green-500"><?php esc_html_e('Plataforma de lista de animes', 'thunay'); ?></h2>
 
             <h3 class="max-w-[30ch] text-center text-white text-lg leading-7"><?php esc_html_e('Explore, descubra e encontre seus animes e mangás favoritos.', 'thunay') ?></h3>
+        </div>
+
+        <div class="flex justify-center items-center flex-col gap-4 mt-10 bg-neutral-800 p-5 rounded-lg border border-neutral-700 w-full">
+            <div class="flex flex-col">
+                <span class="text-center text-white text-sm leading-7"><?php echo sprintf(esc_html__('%s: Este projeto é de caráter pessoal e não comercial, criado com o objetivo de aprendizado e como parte de um portfólio. Todas as informações sobre os animes são obtidas através da API do %s e serão mantidas na língua oficial da API e do site (inglês). Este site não tem fins lucrativos e não visa compartilhar informações de maneira abrangente, mas sim demonstrar habilidades técnicas e conhecimentos adquiridos. Agradecemos pela compreensão e pelo apoio!', 'thunay'), "<span class='font-semibold'>Importante</span>", "<a class='text-green-400 hover:text-green-500 transition duration-300 ease-in' href='https://anilist.co/' target='_blank')>AniList (anilist.co)</a>"); ?></span>
+            </div>
         </div>
     </div>
 

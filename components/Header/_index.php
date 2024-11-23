7b8a486928a24ab2ad9ef84fe3ab83ba779488b5
Thunay Moreira de Soares <thunaymoreiradesoares@gmail.com.br>
2024-11-23 00:54:48 -0300

    🩹

--- components/Header/_index.php
+++ components/Header/_index.php
@@ -1,7 +1,7 @@
 <?php
 
 if (!defined('ABSPATH')) {
-    exit;
+   exit;
 }
 
 ?>
@@ -10,66 +10,66 @@ if (!defined('ABSPATH')) {
 <html <?php language_attributes(); ?>>
 
 <head>
-    <?php
+   <?php
 
-    wp_head();
+   wp_head();
 
-    ?>
+   ?>
 </head>
 
-<body <?php body_class('bg-neutral-900'); ?>>
-    <?php
+<body <?php body_class("bg-neutral-900"); ?>>
+   <?php
 
-    wp_body_open();
+   wp_body_open();
 
-    ?>
+   ?>
 
-    <header id="header" class="fixed top-0 left-0 w-full z-10 bg-neutral-800 border-b-2 border-green-500" x-data="header">
-        <div class="custom-container fade-in-3 transition duration-[3500ms] relative z-50">
-            <div class="flex justify-between items-center py-4">
-                <button x-on:click="displayMenu = !displayMenu">
-                    <img class="w-8" x-bind:src="displayMenu ? '<?php echo get_template_directory_uri(); ?>/assets/images/close.svg' : '<?php echo get_template_directory_uri(); ?>/assets/images/hamburguer.svg'" />
-                </button>
+   <header id="header" class="fixed top-0 left-0 w-full z-10 bg-neutral-800 border-b-2 border-green-500" x-data="header">
+      <div class="custom-container fade-in-3 transition duration-[3500ms] relative z-50">
+         <div class="flex justify-between items-center py-4">
+            <button x-on:click="displayMenu = !displayMenu">
+               <img class="w-8" x-bind:src="displayMenu ? '<?php echo get_template_directory_uri(); ?>/assets/images/close.svg' : '<?php echo get_template_directory_uri(); ?>/assets/images/hamburguer.svg'" />
+            </button>
 
-                <span class="text-white text-lg font-poppins">
-                    Thunay Moreira de Soares
-                </span>
+            <span class="text-white text-lg font-poppins">
+               Thunay Moreira de Soares
+            </span>
 
-                <?php
+            <?php
 
-                if (is_page()) {
+            if (is_page()) {
 
-                ?>
-                    <h1 class="text-white text-lg font-poppins">
-                        <?php esc_html_e(get_the_title()); ?>
-                    </h1>
-                <?php
+            ?>
+               <h1 class="text-white text-lg font-poppins">
+                  <?php esc_html_e(get_the_title()); ?>
+               </h1>
+            <?php
 
-                }
+            }
 
-                ?>
-            </div>
-        </div>
+            ?>
+         </div>
+      </div>
 
-        <div x-bind:class="displayMenu ? 'block' : 'hidden'">
-            <div class="absolute top-0 left-0 w-screen h-screen bg-black opacity-50 blur-xl"></div>
+      <div x-bind:class="displayMenu ? 'block' : 'hidden'">
+         <div class="absolute top-0 left-0 w-screen h-screen bg-black opacity-50 blur-xl"></div>
 
-            <div class="fixed top-36 left-1/2 left-0 -translate-x-2/4 flex flex-col gap-8 opacity-100">
-                <h1 class="flex flex-col items-center text-5xl font-poppins font-semibold text-green-500">
-                    Menu
-                </h1>
+         <div class="fixed top-36 left-1/2 left-0 -translate-x-2/4 flex flex-col gap-8 opacity-100">
+            <h1 class="flex flex-col items-center text-5xl font-poppins font-semibold text-green-500">
+               Menu
+            </h1>
 
-                <?php
+            <?php
 
-                if (is_home()) {
-                    get_template_part('components/header/components/home-menu');
-                } else {
-                    get_template_part('components/header/components/page-menu');
-                }
+            if (is_home()) {
+               get_template_part('components/header/components/home-menu');
+            } else {
+               get_template_part('components/header/components/page-menu');
+            }
 
-                ?>
-            </div>
-        </div>
-    </header>
+            ?>
+         </div>
+      </div>
+   </header>
 
-    <main x-bind:class="displayMenu ? 'blur' : ''">
+   <main x-bind:class="displayMenu ? 'blur' : ''">

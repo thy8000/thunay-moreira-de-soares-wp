c1df57f7a950217f38bf483b7f4361bb9edc6cb8
Thunay Moreira de Soares <thunaymoreiradesoares@gmail.com.br>
2024-11-23 00:54:03 -0300

    🐛 ScrollMagic: Execute script only in home page

--- assets/scripts/pages/home/scrollMagic.js
+++ assets/scripts/pages/home/scrollMagic.js
@@ -1,52 +1,56 @@
-document.addEventListener("DOMContentLoaded", () => {
-  var scrollMagicController = new ScrollMagic.Controller();
+document.addEventListener('DOMContentLoaded', () => {
+   if (!document.body.classList.contains('home')) {
+      return
+   }
 
-  var scrollMagicScene = new ScrollMagic.Scene({
-    triggerElement: "#header .fade-in-3",
-    reverse: false,
-  })
-    .setClassToggle("#header .fade-in-3", "show")
-    .addTo(scrollMagicController);
+   var scrollMagicController = new ScrollMagic.Controller()
 
-  scrollMagicScene = new ScrollMagic.Scene({
-    triggerElement: "#top-hero .fade-in-3",
-    reverse: false,
-  })
-    .setClassToggle("#top-hero .fade-in-3", "show")
-    .addTo(scrollMagicController);
+   var scrollMagicScene = new ScrollMagic.Scene({
+      triggerElement: '#header .fade-in-3',
+      reverse: false,
+   })
+      .setClassToggle('#header .fade-in-3', 'show')
+      .addTo(scrollMagicController)
 
-  scrollMagicScene = new ScrollMagic.Scene({
-    triggerElement: "#about-me .fade-in",
-    reverse: false,
-  })
-    .setClassToggle("#about-me .fade-in", "show")
-    .addTo(scrollMagicController);
+   scrollMagicScene = new ScrollMagic.Scene({
+      triggerElement: '#top-hero .fade-in-3',
+      reverse: false,
+   })
+      .setClassToggle('#top-hero .fade-in-3', 'show')
+      .addTo(scrollMagicController)
 
-  scrollMagicScene = new ScrollMagic.Scene({
-    triggerElement: "#services .fade-in",
-    reverse: false,
-  })
-    .setClassToggle("#services .fade-in", "show")
-    .addTo(scrollMagicController);
+   scrollMagicScene = new ScrollMagic.Scene({
+      triggerElement: '#about-me .fade-in',
+      reverse: false,
+   })
+      .setClassToggle('#about-me .fade-in', 'show')
+      .addTo(scrollMagicController)
 
-  scrollMagicScene = new ScrollMagic.Scene({
-    triggerElement: "#skills .fade-in",
-    reverse: false,
-  })
-    .setClassToggle("#skills .fade-in", "show")
-    .addTo(scrollMagicController);
+   scrollMagicScene = new ScrollMagic.Scene({
+      triggerElement: '#services .fade-in',
+      reverse: false,
+   })
+      .setClassToggle('#services .fade-in', 'show')
+      .addTo(scrollMagicController)
 
-  scrollMagicScene = new ScrollMagic.Scene({
-    triggerElement: "#experience .fade-in",
-    reverse: false,
-  })
-    .setClassToggle("#experience .fade-in", "show")
-    .addTo(scrollMagicController);
+   scrollMagicScene = new ScrollMagic.Scene({
+      triggerElement: '#skills .fade-in',
+      reverse: false,
+   })
+      .setClassToggle('#skills .fade-in', 'show')
+      .addTo(scrollMagicController)
 
-  scrollMagicScene = new ScrollMagic.Scene({
-    triggerElement: "#projects .fade-in-2",
-    reverse: false,
-  })
-    .setClassToggle("#projects .fade-in-2", "show")
-    .addTo(scrollMagicController);
-});
+   scrollMagicScene = new ScrollMagic.Scene({
+      triggerElement: '#experience .fade-in',
+      reverse: false,
+   })
+      .setClassToggle('#experience .fade-in', 'show')
+      .addTo(scrollMagicController)
+
+   scrollMagicScene = new ScrollMagic.Scene({
+      triggerElement: '#projects .fade-in-2',
+      reverse: false,
+   })
+      .setClassToggle('#projects .fade-in-2', 'show')
+      .addTo(scrollMagicController)
+})

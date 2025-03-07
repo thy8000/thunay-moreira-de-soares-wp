16e9deb0e06da5485cfb4e3163615588670f7122
thy8000 <thunaymoreiradesoares@gmail.com>
2025-03-06 22:55:01 -0300

    ♻️ ThemeOptions - Home: use get_field instead custom ACF class

--- includes/entities/ThemeOptions/Home.php
+++ includes/entities/ThemeOptions/Home.php
@@ -1,123 +1,116 @@
 <?php
 
 if (!defined('ABSPATH')) {
-    exit;
+   exit;
 }
 
 class ThemeOptionsHome
 {
-    protected $ACF;
+   public function get_title()
+   {
+      return get_field('home_hero_title', 'option');
+   }
 
-    public function __construct()
-    {
-        $this->ACF = new ACF_Register();
-    }
+   public function get_description()
+   {
+      return get_field('home_hero_description', 'option');
+   }
 
-    public function get_title()
-    {
-        return $this->ACF->get_option_page_field('home_hero_title');
-    }
+   public function get_social_share()
+   {
+      $social_media_list = [
+         'github' => [
+            'link' =>  get_field('home_hero_github', 'option'),
+            'image' => get_template_directory_uri() . '/assets/images/github.svg',
+         ],
+         'linkedin' => [
+            'link' => get_field('home_hero_linkedin', 'option'),
+            'image' => get_template_directory_uri() . '/assets/images/linkedin.svg',
+         ],
+         'email' => [
+            'link' => get_field('home_hero_email', 'option'),
+            'image' => get_template_directory_uri() . '/assets/images/mail.svg',
+         ],
+         'curriculum' => [
+            'link' => get_field('home_hero_curriculum', 'option'),
+            'image' => get_template_directory_uri() . '/assets/images/cv.svg',
+         ],
+         'whatsapp' => [
+            'link' => get_field('home_hero_whatsapp', 'option'),
+            'image' => get_template_directory_uri() . '/assets/images/whatsapp.svg',
+         ],
+      ];
 
-    public function get_description()
-    {
-        return $this->ACF->get_option_page_field('home_hero_description');
-    }
+      $output = '';
 
-    public function get_social_share()
-    {
-        $social_media_list = [
-            'github' => [
-                'link' => $this->ACF->get_option_page_field('home_hero_github'),
-                'image' => get_template_directory_uri() . '/assets/images/github.svg',
-            ],
-            'linkedin' => [
-                'link' => $this->ACF->get_option_page_field('home_hero_linkedin'),
-                'image' => get_template_directory_uri() . '/assets/images/linkedin.svg',
-            ],
-            'email' => [
-                'link' => $this->ACF->get_option_page_field('home_hero_email'),
-                'image' => get_template_directory_uri() . '/assets/images/mail.svg',
-            ],
-            'curriculum' => [
-                'link' => $this->ACF->get_option_page_field('home_hero_curriculum'),
-                'image' => get_template_directory_uri() . '/assets/images/cv.svg',
-            ],
-            'whatsapp' => [
-                'link' => $this->ACF->get_option_page_field('home_hero_whatsapp'),
-                'image' => get_template_directory_uri() . '/assets/images/whatsapp.svg',
-            ],
-        ];
+      $social_media_output = array_map(function ($social_media) {
+         if (empty($social_media)) {
+            return '';
+         }
 
-        $output = '';
-
-        $social_media_output = array_map(function ($social_media) {
-            if (empty($social_media)) {
-                return '';
-            }
-
-            return <<<OUTPUT
+         return <<<OUTPUT
             <a class="flex justify-center items-center w-16 h-16 border-2 border-neutral-700 bg-neutral-800 rounded p-3 hover:border-green-500" href="{$social_media['link']}" target="_blank">
                 <img class="w-10" src="{$social_media['image']}" />
             </a>
             OUTPUT;
-        }, $social_media_list);
+      }, $social_media_list);
 
-        $output = implode('', array_filter($social_media_output));
+      $output = implode('', array_filter($social_media_output));
 
-        if (empty($output)) {
-            return;
-        }
+      if (empty($output)) {
+         return;
+      }
 
-        return "<div class='fade-in-3 transition duration-[4500ms] flex flex-wrap justify-center items-center gap-8'>" . $output . "</div>";
-    }
+      return "<div class='fade-in-3 transition duration-[4500ms] flex flex-wrap justify-center items-center gap-8'>" . $output . "</div>";
+   }
 
-    public function get_about_me_photo()
-    {
-        return $this->ACF->get_option_page_field('home_about_me_photo');
-    }
+   public function get_about_me_photo()
+   {
+      return get_field('home_about_me_photo', 'option');
+   }
 
-    public function get_about_me_name()
-    {
-        return $this->ACF->get_option_page_field('home_about_me_name');
-    }
+   public function get_about_me_name()
+   {
+      return get_field('home_about_me_name', 'option');
+   }
 
-    public function get_about_me_job()
-    {
-        return $this->ACF->get_option_page_field('home_about_me_job');
-    }
+   public function get_about_me_job()
+   {
+      return get_field('home_about_me_job', 'option');
+   }
 
-    public function get_about_me_description()
-    {
-        return $this->ACF->get_option_page_field('home_about_me_description');
-    }
+   public function get_about_me_description()
+   {
+      return get_field('home_about_me_description', 'option');
+   }
 
-    public function get_services_text()
-    {
-        return $this->ACF->get_option_page_field('home_services_text');
-    }
+   public function get_services_text()
+   {
+      return get_field('home_services_text', 'option');
+   }
 
-    public function get_services_cards()
-    {
-        return $this->ACF->get_option_page_field('home_services_cards');
-    }
+   public function get_services_cards()
+   {
+      return get_field('home_services_cards', 'option');
+   }
 
-    public function get_skills_description()
-    {
-        return $this->ACF->get_option_page_field('home_skills_description');
-    }
+   public function get_skills_description()
+   {
+      return get_field('home_skills_description', 'option');
+   }
 
-    public function get_skills_list()
-    {
-        return $this->ACF->get_option_page_field('home_skills_skills_list');
-    }
+   public function get_skills_list()
+   {
+      return get_field('home_skills_skills_list', 'option');
+   }
 
-    public function get_experience_text()
-    {
-        return $this->ACF->get_option_page_field('home_experience_text');
-    }
+   public function get_experience_text()
+   {
+      return get_field('home_experience_text', 'option');
+   }
 
-    public function get_experience_timeline()
-    {
-        return $this->ACF->get_option_page_field('home_experience_timeline');
-    }
+   public function get_experience_timeline()
+   {
+      return get_field('home_experience_timeline', 'option');
+   }
 }

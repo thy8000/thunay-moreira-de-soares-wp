a681ffbe038d8be2e6c32d2eb105a629dcac9567
thy8000 <thunaymoreiradesoares@gmail.com>
2025-03-18 22:31:42 -0300

    🔧 Hide Posts from WP-Admin

--- includes/global/GlobalOptions.php
+++ includes/global/GlobalOptions.php
@@ -1,47 +1,54 @@
 <?php
 
 if (!defined('ABSPATH')) {
-    exit;
+   exit;
 }
 
 class GlobalOptions
 {
-    public $mimes = [
-        'svg' => 'image/svg+xml',
-        'png' => 'image/png',
-    ];
+   public $mimes = [
+      'svg' => 'image/svg+xml',
+      'png' => 'image/png',
+   ];
 
-    public function __construct()
-    {
-        add_action('wp_head', [$this, 'register_head_content'], 1);
+   public function __construct()
+   {
+      add_action('wp_head', [$this, 'register_head_content'], 1);
 
-        add_filter('upload_mimes', [$this, 'register_svg_support']);
+      add_filter('upload_mimes', [$this, 'register_svg_support']);
 
-        add_action('init', [$this, 'register_theme_support']);
-    }
+      add_action('init', [$this, 'register_theme_support']);
 
-    public function register_head_content()
-    {
-        echo <<<OUTPUT
+      add_action('admin_menu', [$this, 'disable_posts_admin']);
+   }
+
+   public function register_head_content()
+   {
+      echo <<<OUTPUT
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         OUTPUT;
-    }
+   }
 
-    public function register_svg_support()
-    {
-        foreach ($this->mimes as $key => $mime) {
-            $mimes[$key] = $mime;
-        }
+   public function register_svg_support()
+   {
+      foreach ($this->mimes as $key => $mime) {
+         $mimes[$key] = $mime;
+      }
 
-        return $mimes;
-    }
+      return $mimes;
+   }
 
-    public function register_theme_support()
-    {
-        add_theme_support('post-thumbnails');
-    }
+   public function register_theme_support()
+   {
+      add_theme_support('post-thumbnails');
+   }
+
+   public function disable_posts_admin()
+   {
+      remove_menu_page('edit.php');
+   }
 }
 
 new GlobalOptions();

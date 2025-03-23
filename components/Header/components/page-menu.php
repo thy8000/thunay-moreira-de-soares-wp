55cd3bd772c4a268fe5ec5576a63cb03451016db
thy8000 <thunaymoreiradesoares@gmail.com>
2025-03-22 23:18:37 -0300

    ✨ MenuItem - get custom name by template

--- components/Header/components/page-menu.php
+++ components/Header/components/page-menu.php
@@ -16,9 +16,18 @@ if (empty($menu_items)) {
    <?php
 
    foreach ($menu_items as $menu_item) {
+      $home_menu_alternative_name_list = get_field('home_menu_alternative_name_list', $menu_item->ID);
+
+      if (empty($home_menu_alternative_name_list)) {
+         $home_menu_alternative_name = $menu_item->post_title;
+      } else {
+         $home_menu_alternative_name = thunay_get_page_item($home_menu_alternative_name_list, get_the_ID());
+
+         $home_menu_alternative_name = $home_menu_alternative_name['name'] ?? $menu_item->post_title;
+      }
 
    ?>
-      <button id="<?php echo esc_attr($menu_item->post_name); ?>" class="font-semibold text-3xl text-white hover:scale-150 transition-all duration-500 ease-linear" x-on:click="goToElement('<?php esc_attr_e(get_field('home_menu_element_ID', $menu_item->ID)); ?>')"><?php esc_html_e($menu_item->post_title); ?></button>
+      <button id="<?php echo esc_attr($menu_item->post_name); ?>" class="font-semibold text-3xl text-white hover:scale-150 transition-all duration-500 ease-linear" x-on:click="goToElement('<?php esc_attr_e(get_field('home_menu_element_ID', $menu_item->ID)); ?>')"><?php echo esc_html($home_menu_alternative_name); ?></button>
    <?php
 
    }
